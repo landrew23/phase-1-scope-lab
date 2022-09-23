97573d16e8e0e92cdfc08a0b6562731c628c95b7
William Killett <williamkillettiii@yahoo.com>
2022-09-22 20:15:21 -0700

    commit tree 2d54f6593690ca851ff89057d878bd83673d9f53

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = "bob"
+function Fix_the_Scope(windy){
+
+}
+
+function upperCaseCustomerName(wildow){
+customerName = customerName.toUpperCase(customerName);
+return customerName
+}
+
+function setBestCustomer(widow){
+ bestCustomer = "not bob" 
+ return bestCustomer
+}
+
+function overwriteBestCustomer(foggy){
+  bestCustomer = "maybe bob"
+ return bestCustomer   
+}
+
+const leastFavoriteCustomer = "true"
+function changeLeastFavoriteCustomer(sunny){
+leastFavoriteCustomer = "false"
+}
+
+
